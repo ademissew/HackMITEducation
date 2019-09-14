7ce80259b75b800a3697ce83d9453757272a1fd2
SonJoseph <joeson34@cs.unc.edu>
2019-09-14 19:17:19 -0400

    teacherhome and classview

--- my-app/src/components/TeacherHome.js
+++ my-app/src/components/TeacherHome.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
 class TeacherHome extends React.Component {
+    constructor(props){
+        super(props)
+        
+        const classes = ['Calculus', 'Geometry']
 
-        render(){
-            return <div>HI</div>
-        }
+        this.state = {'classes': classes }
+    }
 
+    /*async getDataAxios(){
+        const response =
+          await axios.get("https://dog.ceo/api/breeds/list/all",
+              { params: {name: 'bruno'}}
+          )
+        console.log(response.data)
+    }*/
+
+    render(){
+        return (
+            <div>
+                {<ul>
+                    {this.state.classes.map((item)=><button> {item} </button>)}
+                </ul>}
+                <button> Create a class </button>
+            </div>
+        )
+    }
 }
 
 export default TeacherHome;
