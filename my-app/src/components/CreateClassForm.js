f8f8dba72f15c502565503f10dc188f7aee48f7c
Alenta Demissew <alenta@Alentas-MacBook-Pro.local>
2019-09-15 01:34:15 -0400

    added created classes style

--- my-app/src/components/CreateClassForm.js
+++ my-app/src/components/CreateClassForm.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import Container from './Container'
 import { createClass } from '../api'
+import {Button,Fab, TextField} from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add';
 
 class CreateClassForm extends React.Component {
     constructor(props) {
@@ -8,17 +11,23 @@ class CreateClassForm extends React.Component {
         this.addStudent = this.addStudent.bind(this)
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleEnter = this.handleEnter.bind(this)
+
+    }
+    handleEnter(e){
+        if (e.keyCode == 13){
+        this.setState({ students: this.state.students.concat(this.state.student),student:"" })
+        document.getElementById("student-name-id").value = "HI"
+        }
     }
 
-    addStudent() {
-        const student = document.getElementById("student-name-id").value
-        console.log(student)
-        this.setState({ students: this.state.students.concat(student) })
-        document.getElementById("student-name-id").value = ""
+    addStudent(e) {
+        e.preventDefault();
+        this.setState({ student: e.target.value });
     }
 
     handleChange(event) {
-        this.setState({ students: this.state.students, className: event.target.value });
+        this.setState({ className: event.target.value });
     }
 
     handleSubmit(event) {
@@ -35,23 +44,41 @@ class CreateClassForm extends React.Component {
     }
 
     render() {
-        return (<div >
-            <label>
-                Class Name:
-                    <input type="text" name="class-name" value={this.state.className} onChange={this.handleChange} />
-            </label>
-            <div></div>
-            <ul>
-                {this.state.students.map((student) => <li key={student}>{student}</li>)}
-            </ul>
-            <label>
-                StudentId:
-                    <input type="text" name="student-name" id="student-name-id" />
-                <button name="add-student" onClick={this.addStudent}>+</button>
-            </label>
-            <div></div>
-            <button type="submit" onClick={this.handleSubmit}>Create</button>
-        </div >)
+        return (
+            <Container>
+                <TextField
+                    required
+                    id="class-name-id"
+                    label="Class Name:"
+                    placeholder="Class Name"
+                    margin="normal"
+                    style={{width:"50%"}}
+                    name="class-name" 
+                    onChange={this.handleChange} 
+                    />
+                <ul>
+                    {this.state.students.map((student) => <li key={student}>{student}</li>)}
+                </ul>
+                    
+                    <TextField
+                        required
+                        id="student-name-id"
+                        label="Add Student:"
+                        placeholder="Student ID"
+                        margin="normal"
+                        style={{width:"50%",textAlign:"left"}}
+                        name="student-name" 
+                        value={this.state.student} 
+                        variant="outlined"
+                        onChange={this.addStudent} 
+                        onKeyDown={this.handleEnter}
+                        />
+                    
+                        {/* <AddIcon color="disabled" style={{width:"10%",display:'flex',paddingTop:"5%"}}/> */}
+
+                <Button variant="outlined" type="submit" onClick={this.handleSubmit}>Create </Button>
+            </Container>
+        )
     }
 }
 
