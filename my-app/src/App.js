cdfe5445811d12ec7600d9c4da00798596c9e8cb
Alenta Demissew <alenta@Alentas-MacBook-Pro.local>
2019-09-14 20:47:09 -0400

    changed favicon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import TeacherHome from './components/TeacherHome'
 import CreateClassForm from './components/CreateClassForm'
